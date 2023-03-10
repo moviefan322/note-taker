cdd4febddab959786856cf75709de5788dbac0f7
moviefan322 <philman202@yahoo.com>
2023-03-10 10:01:44 -0500

    homepage set, api get routes in place

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //DEPENDENCIES
 const express = require("express");
 const path = require("path");
+const db = require("./db/db.json");
 
 //DATA
 
@@ -15,10 +16,21 @@ app.use(express.urlencoded({ extended: false }));
 
 //ROUTES
 
-app.get('/api/notes', (req,res) => {
+//homepage
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-})
+//retrieve all notes
+app.get("/api/notes", (req, res) => {
+  res.json(db);
+});
 
+//retrieve one note
+app.get("/api/notes/:index", (req, res) => {
+  const { index } = req.params;
+  res.json(db[Number(index)]);
+});
 
 //START SERVER
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
