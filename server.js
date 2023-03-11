3f04bbef84a28de459d470e5ff28453df7c9e7d7
moviefan322 <philman202@yahoo.com>
2023-03-10 19:28:54 -0500

    enables app.post

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const path = require("path");
 const db = require("./db/db.json");
+const { writeFile } = require("fs");
 
 //DATA
 
@@ -15,10 +16,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //ROUTES
+// static folder
+// app.use(express.static(path.join(__dirname, "public")));
 
 //homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+//notes page
+app.get("/notes.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 //retrieve all notes
@@ -30,6 +37,23 @@ app.get("/api/notes", (req, res) => {
 app.get("/api/notes/:index", (req, res) => {
   const { index } = req.params;
   res.json(db[Number(index)]);
+});
+
+//post note
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  const newNote = {
+    title,
+    text,
+  };
+  db.push(newNote);
+  writeFile("./db/db.json", JSON.stringify(db), (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("File written successfully\n");
+    }
+  });
+  res.json(db);
 });
 
 //START SERVER
